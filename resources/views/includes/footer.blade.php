7ce43aff5a64903c471ac89176a26b4f924a0536
viniciusxande30 <vinicius.xande30@gmail.com>
2023-02-06 19:43:01 -0300

    Subir arquivos

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,50 @@
+<section class="section bg-color-dark-scale-2 border-0 m-0 py-4">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <ul class="list list-unstyled list-inline d-flex align-items-center justify-content-center flex-column flex-lg-row mb-0">
+          <li class="list-inline-item custom-text-color-1 color-inherit mb-lg-0 text-2 pe-2">Alguns de Nossos Links</li>
+          <li class="list-inline-item mb-lg-0"><a href="{{url('/')}}" style="background-color:#E7BE17" class="btn btn-dark btn-modern btn-rounded btn-px-4 py-3 border-0">Página Inicial</a></li>
+          <li class="list-inline-item mb-lg-0"><a href="{{url('/')}}/#por-que-investir" style="background-color:#E7BE17"  class="btn btn-dark btn-modern btn-rounded btn-px-4 py-3 border-0">Por que Investir?</a></li>
+		  <li class="list-inline-item mb-lg-0"><a href="{{url('/')}}/#simulador" style="background-color:#E7BE17"  class="btn btn-dark btn-modern btn-rounded btn-px-4 py-3 border-0">Faça Sua Simulação</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+
+			<footer id="footer" class="bg-color-dark-scale-2 border border-end-0 border-start-0 border-bottom-0 border-color-light-3 mt-0">
+				<div class="container text-center my-3 py-5">
+					<a href="https://umtudoso.com.br">
+						<img src="img/lazy.png" data-src="img/umtudoso.webp" width="200" class="mb-4 appear-animation lazyload" alt="Porto" data-appear-animation="fadeIn" data-appear-animation-delay="300">
+					</a>
+					<p class="text-4 mb-4">Faça sua Cotação Totalmente Gratuita, Aproveite!</p>
+				</div>
+				<div class="copyright bg-dark py-4">
+					<div class="container text-center py-2">
+						<p class="mb-0 text-2">Copyright 2023 Um Tudo Só - Todos os Direitos Reservados</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+		<!-- Vendor -->
+		<script src="{{url('/')}}/vendor/plugins/js/plugins.min.js"></script>
+		<script src="{{url('/')}}/vendor/gsap/gsap.min.js"></script>
+		<script src="{{url('/')}}/vendor/gsap/ScrollTrigger.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="{{url('/')}}/js/theme.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="{{url('/')}}/js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="{{url('/')}}/js/theme.init.js"></script>
+
+	<!-- Examples -->
+		<script src="{{url('/')}}/js/examples/examples.carousels.js"></script>
+
+	</body>
+</html>
